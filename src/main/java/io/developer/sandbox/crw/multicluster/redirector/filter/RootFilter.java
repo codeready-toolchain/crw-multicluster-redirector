862953250b58dad5b629ff0ba0e61e8e5ac0a3f1
Ilya Buziuk <ibuziuk@redhat.com>
2025-10-22 11:30:39 +0000

    chore: 0.0.12 release. Migrating to Quarkus 3.8.3
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- src/main/java/io/developer/sandbox/crw/multicluster/redirector/filter/RootFilter.java
+++ src/main/java/io/developer/sandbox/crw/multicluster/redirector/filter/RootFilter.java
@@ -14,12 +14,12 @@ package io.developer.sandbox.crw.multicluster.redirector.filter;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebFilter(urlPatterns = "/*")
 public class RootFilter extends HttpFilter {
--- src/test/java/io/developer/sandbox/crw/multicluster/redirector/config/NativeConfigurationProviderIT.java
+++ src/test/java/io/developer/sandbox/crw/multicluster/redirector/config/NativeConfigurationProviderIT.java
@@ -1,8 +1,8 @@
 package io.developer.sandbox.crw.multicluster.redirector.config;
 
-import io.quarkus.test.junit.NativeImageTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 
-@NativeImageTest
+@QuarkusIntegrationTest
 public class NativeConfigurationProviderIT extends ConfigurationProviderTest {
 
     // Execute the same tests but in native mode.
